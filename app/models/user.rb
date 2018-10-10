5dbb932204ebe2f1fcb09ecaa62193f0e274d398
quocbuingoc <buingocquocbkdn@gmail.com>
2018-10-10 08:59:40 +0700

    Chap 6

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  before_save{email.downcase!}
+  validates :name,  presence: true, length: {maximum: Settings.length_string.name_max}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.length_string.email_max},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: {minimum: Settings.length_string.pass_min}
+end
