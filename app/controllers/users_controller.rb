0cf928815c32d183e154fbf41ed7075c2c4a1901
quocbuingoc <buingocquocbkdn@gmail.com>
2018-10-10 08:59:40 +0700

    Chap 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t("controller.users.create_success")
+      log_in @user
+      flash[:success] = t "controllers.users.create_success"
       redirect_to @user
     else
       render :new
